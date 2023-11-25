46d56b516ebab7613a0db6b29efdab1dc65df733
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2023-11-25 10:01:49 +0100

    poprawki kodu

--- autoDeal/src/main/resources/data.sql
+++ autoDeal/src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO user (firstName) VALUES ('Jan');
-INSERT INTO user (lastName) VALUES ('Kowalski');
-INSERT INTO user (password) VALUES ('abc');
-INSERT INTO user (email) VALUES ('jan.kowalski@gmail.com');
-INSERT INTO user (phone) VALUES ('501-123-456');
+INSERT INTO customer (first_name, last_name, password, email, phone) VALUES ('Jan','Kowalski', 'abc','jan.kowalski@gmail.com', '501-123-456');
+-- INSERT INTO user (lastName) VALUES ('Kowalski');
+-- INSERT INTO user (password) VALUES ('abc');
+-- INSERT INTO user (email) VALUES ('jan.kowalski@gmail.com');
+-- INSERT INTO user (phone) VALUES ('501-123-456');
 
-INSERT INTO user (firstName) VALUES ('Janusz');
-INSERT INTO user (lastName) VALUES ('Kowalski');
-INSERT INTO user (password) VALUES ('xyx');
-INSERT INTO user (email) VALUES ('janusz.kowalski@gmail.com');
-INSERT INTO user (phone) VALUES ('501-789-456');
+-- INSERT INTO user (firstName) VALUES ('Janusz');
+-- INSERT INTO user (lastName) VALUES ('Kowalski');
+-- INSERT INTO user (password) VALUES ('xyx');
+-- INSERT INTO user (email) VALUES ('janusz.kowalski@gmail.com');
+-- INSERT INTO user (phone) VALUES ('501-789-456');
